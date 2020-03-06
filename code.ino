faba87a67df738cc64990113f661bc771f96a2c4
pico7 <ebrunet@picosoft.ca>
2020-03-06 10:26:27 -0500

    Update code.ino

--- code.ino
+++ code.ino
@@ -6,7 +6,8 @@
 * References:
 * - https://www.maxbotix.com/tutorials6/032-using-analog-voltage-pin-3.htm
 **********************************************************************************************/
-const int maxSonarSensor = A2; 
+const int sourceVoltage = A0; 
+const int maxSonarSensor = A1; 
 const int band = 1024;
 
 float Vcc, V5mm, maxSonarSensor_mV, Rmm, Rcm;
@@ -17,33 +18,28 @@ void setup() {
 }
 
 void readSensor(){
-    Vcc = 3.25; // may vary and should be measured 
-    V5mm = ((Vcc / band) * 1000); // convert to mV
+    Vcc = analogRead(sourceVoltage);    
+    V5mm = (Vcc / band);
 
     maxSonarSensor_mV = analogRead(maxSonarSensor);
     
     Rmm = ((maxSonarSensor_mV / V5mm) * 5);
-    Rcm = (Rmm / 10); // conver to cm
+    Rcm = (Rmm / 10); // convert to cm
 }
 
 void printRange(){
     Serial.print(maxSonarSensor);
-    Serial.print(" Vcc=");
-    Serial.print(Vcc);
-    Serial.print(" V5mm=");
-    Serial.print(V5mm);
-    Serial.print(" maxSonarSensor_mV=");
-    Serial.print(maxSonarSensor_mV);
-    Serial.print(" Rmm=");
-    Serial.print(Rmm);
-    Serial.print(" Rcm=");
-    Serial.println(Rcm); 
+    Serial.printf(" Vcc=%.2f", Vcc);
+    Serial.printf(" V5mm=%.4f", V5mm);
+    Serial.printf(" sourceVoltage_mV=%.2f", sourceVoltage_mV);
+    Serial.printf(" maxSonarSensor_mV=%.2f", maxSonarSensor_mV);
+    Serial.printf(" Rmm=%.2f", Rmm);
+    Serial.printf(" Rcm=%.2f", Rcm);
+    Serial.println(); 
 }
 
-void loop() {
-    
+void loop() {    
     readSensor();
-    printRange();
-    
+    printRange();    
     delay(2000);
 }
